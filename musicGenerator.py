c6b9301248684760ff742c9fae3fb5d69c3c93d4
MAFiA <mafia303@gmail.com>
2017-10-03 18:44:49 -0700

    Update musicGenerator.py

--- musicGenerator.py
+++ musicGenerator.py
@@ -1,7 +1,8 @@
 from random import randint, shuffle
-def nar():
-    rn = randint(0,1) # major or Minor
-    ar = [0,3+rn,7,12,15+rn,19,24,27+rn]
+# random selector for:
+def nar(): # major or Minor
+    rn = randint(0,1)
+    ar = [0,3+rn,7,12,15+rn,19,24,27+rn] #arpeggio sequence
     return(ar)
 def step():
     step = randint(0,3)
@@ -13,11 +14,13 @@ def step3():
     step = randint(1,3)
     return(step)
 
-dim = [-3,0,3,6,9,12,15,18,21,24]
-aug = [0,4,8,12,16,20,24]
+dim = [-3,0,3,6,9,12,15,18,21,24] #diminished sequence
+aug = [0,4,8,12,16,20,24] # augmented sequence
 
-nums = [0,2,3,5,7,8,10,12,14,15,17]
+nums = [0,2,3,5,7,8,10,12,14,15,17] #scale
 
+
+#generating the sequences
 arp=[]
 for i in range(0,5,1):
     st = step() #start note on the arpeggio
@@ -80,3 +83,46 @@ for i in range(0,len(arp)-2,1):
     if(i==len(arp)-1-2):
         tii = .6
     lengl.extend([.13*ti+tii])
+
+
+# %%
+#==============================================================================
+#  # Music player
+#==============================================================================
+import pyaudio
+
+def sine(frequency, length, rate, f2=0):
+    length = int(length * rate)
+    factor = float(frequency) * (np.pi * 2) / rate
+    factor2 = float(f2) * (np.pi * 2) / rate
+    return np.sin(np.arange(length) * factor) + np.sin(np.arange(length) * factor2)
+
+
+def play_tone(stream, frequency=440, length=0.1, rate=44100, f2=0):
+    chunks = []
+    chunks.append(sine(frequency, length, rate,f2))
+
+    chunk = np.concatenate(chunks) * 0.25
+
+    stream.write(chunk.astype(np.float32).tostring())
+
+
+if __name__ == '__main__':
+    p = pyaudio.PyAudio()
+    stream = p.open(format=pyaudio.paFloat32,
+                    channels=1, rate=44100, output=1)
+    
+
+
+
+    for i in range(0,len(arp)-2,1):
+        play_tone(stream,frequency=330*np.power(2,arp[i]/12),f2=165*np.power(2,arp[i+2]/12),length=.8*lengl[i])
+
+        
+#    for i in range(0,len(arp),1):
+#        play_tone(stream,frequency=440*np.power(2,arp[i]/12))
+        
+        
+        
+    stream.close()
+    p.terminate()
